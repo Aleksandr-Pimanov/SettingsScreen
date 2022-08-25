f6e27bf2cf30b066e4524ddff734632e6337680f
Aleksandr Pimanov <pimanov.aleksandr00@mail.ru>
2022-08-25 17:22:58 +0400

    add model for custom tableViewCell

--- SettingsScreen/CustomModule/CustomTableViewCell.swift
+++ SettingsScreen/CustomModule/CustomTableViewCell.swift
@@ -2,28 +2,80 @@
 //  CustomTableViewCell.swift
 //  SettingsScreen
 //
-//  Created by новый пользователь on 25.08.2022.
+//  Created by Aleksandr Pimanov on 25.08.2022.
 //
 
 import UIKit
 
-class CustomTableViewCell: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+class CustomTableViewCell: UITableViewCell {
+    
+    var itemCell: ItemsCell? {
+        didSet {
+            leftIcon.image = itemCell?.leftIcon
+            cellTitle.text = itemCell?.cellTitle
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private let leftIcon: UIImageView = {
+        let icon = UIImageView()
+        icon.clipsToBounds = true
+        icon.translatesAutoresizingMaskIntoConstraints = false
+        return icon
+    }()
+    
+    private let cellTitle: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 15)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let iconContainer: UIView = {
+        let container = UIView()
+        container.translatesAutoresizingMaskIntoConstraints = false
+        return container
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupHierarchy()
+        setupContainerLayout()
+        setupLeftIconLayout()
+        setupTitleLayout()
     }
-    */
-
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    private func setupHierarchy() {
+        
+    }
+    
+    private func setupContainerLayout() {
+        NSLayoutConstraint.activate([
+            iconContainer.topAnchor.constraint(equalTo: contentView.topAnchor),
+            iconContainer.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            iconContainer.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            iconContainer.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            iconContainer.widthAnchor.constraint(equalToConstant: 20),
+            iconContainer.heightAnchor.constraint(equalToConstant: 20)
+        ])
+    }
+    
+    private func setupLeftIconLayout() {
+        NSLayoutConstraint.activate([
+            leftIcon.topAnchor.constraint(equalTo: iconContainer.topAnchor),
+            leftIcon.trailingAnchor.constraint(equalTo: iconContainer.trailingAnchor),
+            leftIcon.leadingAnchor.constraint(equalTo: iconContainer.leadingAnchor),
+            leftIcon.bottomAnchor.constraint(equalTo: iconContainer.bottomAnchor)
+        ])
+    }
+    
+    private func setupTitleLayout() {
+        NSLayoutConstraint.activate([
+            cellTitle.centerYAnchor.constraint(equalTo: iconContainer.centerYAnchor),
+            cellTitle.leadingAnchor.constraint(equalTo: iconContainer.leadingAnchor, constant: 20)
+        ])
+    }
 }
--- SettingsScreen/CustomModule/Model.swift
+++ SettingsScreen/CustomModule/Model.swift
@@ -2,7 +2,32 @@
 //  Model.swift
 //  SettingsScreen
 //
-//  Created by новый пользователь on 25.08.2022.
+//  Created by Aleksandr Pimanov on 25.08.2022.
 //
 
-import Foundation
+import UIKit
+
+struct ItemsCell: Hashable {
+    var leftIcon: UIImage
+    var cellTitle: String
+}
+
+extension ItemsCell {
+    
+    static var items: [[ItemsCell]] = [
+        [ItemsCell(leftIcon: UIImage(systemName: "airplane")!, cellTitle: "Aвиарежим")],
+        [ItemsCell(leftIcon: UIImage(systemName: "wifi")!, cellTitle: "Wi-Fi")],
+        [ItemsCell(leftIcon: UIImage(named: "bluetooth")!, cellTitle: "Bluetooth")],
+        [ItemsCell(leftIcon: UIImage(systemName: "antenna.radiowaves.left.and.right")!, cellTitle: "Сотовая связь")],
+        [ItemsCell(leftIcon: UIImage(systemName: "personalhotspot")!, cellTitle: "Режим модема")],
+        [ItemsCell(leftIcon: UIImage(named: "vpn")!, cellTitle: "VPN")],
+        [ItemsCell(leftIcon: UIImage(named: "notification")!, cellTitle: "Уведомления")],
+        [ItemsCell(leftIcon: UIImage(named: "sound")!, cellTitle: "Звуки, тактильные сигналы")],
+        [ItemsCell(leftIcon: UIImage(systemName: "moon.fill")!, cellTitle: "Не беспокоить")],
+        [ItemsCell(leftIcon: UIImage(named: "time")!, cellTitle: "Экранное время")],
+        [ItemsCell(leftIcon: UIImage(systemName: "gear")!, cellTitle: "Основные")],
+        [ItemsCell(leftIcon: UIImage(systemName: "switch.2")!, cellTitle: "Пункт управления")],
+        [ItemsCell(leftIcon: UIImage(systemName: "textformat.size")!, cellTitle: "Экран и яркость")],
+        [ItemsCell(leftIcon: UIImage(systemName: "figure.arms.open")!, cellTitle: "Универсальный доступ")],
+    ]
+}
